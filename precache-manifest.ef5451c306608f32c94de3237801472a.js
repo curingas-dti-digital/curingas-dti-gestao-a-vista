60229a04f3d126e2357c54600dc9053371059126
thalissoncorreiadti <correia.tssc@gmail.com>
2019-05-22 18:34:02 -0300

    Updates

--- precache-manifest.ef5451c306608f32c94de3237801472a.js
+++ precache-manifest.ef5451c306608f32c94de3237801472a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec5ee21de9bde498bd58e47302a9a0ed",
+    "revision": "f1e6bcb1cbcb27a365e1003eeb1b2d71",
     "url": "/curingas-dti-gestao-a-vista/index.html"
   },
   {
-    "revision": "832404e09cc18e3d73fd",
+    "revision": "166b372a6b7f8484b119",
     "url": "/curingas-dti-gestao-a-vista/static/css/main.498d002a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/curingas-dti-gestao-a-vista/static/js/2.9a93788d.chunk.js"
   },
   {
-    "revision": "832404e09cc18e3d73fd",
-    "url": "/curingas-dti-gestao-a-vista/static/js/main.084972af.chunk.js"
+    "revision": "166b372a6b7f8484b119",
+    "url": "/curingas-dti-gestao-a-vista/static/js/main.7831813d.chunk.js"
   },
   {
     "revision": "b9905a0f369230167948",
