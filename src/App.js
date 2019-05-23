a58273dafb03a71e5e99d9256b088cb952ed44f9
thalissoncorreiadti <correia.tssc@gmail.com>
2019-05-23 00:58:18 -0300

    Timer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core';
 import Retro from './components/Retro/Retro';
 import Eventos from './components/Eventos/Eventos';
 import Theme from './Theme';
 
-function App() {
-  return (
-    <Router>
+class App extends React.Component {
+  state = {
+    page: 'retro'
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ page: this.state.page === 'retro' ? 'eventos' : 'retro' });
+    }, 30 * 1000);
+  }
+  render() {
+    return (
       <MuiThemeProvider theme={Theme}>
-        <Route path="/retro" component={Retro} />
-        <Route path="/eventos" component={Eventos} />
+        {this.state.page === 'retro' && <Retro />}
+        {this.state.page === 'eventos' && <Eventos />}
       </MuiThemeProvider>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
